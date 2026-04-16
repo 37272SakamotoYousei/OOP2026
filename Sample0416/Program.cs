f4f8bda5e53c1972560e11bb882696c9f2032ac7
坂本耀世 <oibc37272@ojs.ac.jp>
2026-04-16 11:54:21 +0900

    for文をwhile文に直した。

--- Sample0416/Program.cs
+++ Sample0416/Program.cs
@@ -4,9 +4,10 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i<10; i++) 
-            { 
-            Console.WriteLine("Hello, World!");
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Hello, World!");
             }
         }
     }
