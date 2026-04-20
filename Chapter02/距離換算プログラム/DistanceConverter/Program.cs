396a6970d52f0348da07eda813909a6e61ef8f42
坂本耀世 <oibc37272@ojs.ac.jp>
2026-04-20 11:42:56 +0900

    p30コード2.3まで入力

---
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+
+namespace DistanceConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0] == "-tom"){
+                //フィートからメートルへの対応表を出力
+                for (int feet = 1; feet <= 10; feet++){
+                    double merter = FeetToMerter(feet);
+                    Console.WriteLine($"{feet}ft = {merter:0.0000}m");
+                }
+            }else{
+                //メートルからフィートへの対応表を出力
+                for (int meter = 1; meter <= 10; meter++){
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+                }
+            }
+        }
+        //フィートからメートルを求める
+        static double FeetToMerter(int feet){
+            return feet * 0.3048;
+        }
+        //メートルからフィートを求める
+        static double MeterToFeet(int meter){
+            return meter / 0.3048;
+        }
+    }
+}
