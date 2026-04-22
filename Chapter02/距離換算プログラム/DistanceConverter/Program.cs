88404abb7efbdd94b30bbcaf3d7766c73476af69
坂本耀世 <oibc37272@ojs.ac.jp>
2026-04-22 09:35:02 +0900

    コマンドライン引数の対応

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -7,26 +7,47 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
-            if (args.Length >= 1 && args[0] == "-tom"){
-                //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++){
-                    double merter = FeetToMerter(feet);
-                    Console.WriteLine($"{feet}ft = {merter:0.0000}m");
-                }
-            }else{
-                //メートルからフィートへの対応表を出力
-                for (int meter = 1; meter <= 10; meter++){
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
-                }
+            if (args.Length >= 1 && args[0] == "-tom")
+            {                                  //メートルへの変換
+                PrintFeetToMeterList(1, 10);
+            }
+            else if(args.Length >= 1 && args[0] == "-tof")
+            {
+                PrintMeterTofeetList(1, 10);
+            }
+            else
+            {
+                Console.WriteLine("引数エラー");
             }
         }
+
+        static void PrintFeetToMeterList(int start, int stop)
+        {
+            //フィートからメートルへの対応表を出力
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double merter = FeetToMerter(feet);
+                Console.WriteLine($"{feet}ft = {merter:0.0000}m");
+            }
+        }
+        static void PrintMeterTofeetList(int start, int stop)
+        {
+            //メートルからフィートへの対応表を出力
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+
         //フィートからメートルを求める
-        static double FeetToMerter(int feet){
+        static double FeetToMerter(int feet)
+        {
             return feet * 0.3048;
         }
         //メートルからフィートを求める
-        static double MeterToFeet(int meter){
+        static double MeterToFeet(int meter)
+        {
             return meter / 0.3048;
         }
     }
