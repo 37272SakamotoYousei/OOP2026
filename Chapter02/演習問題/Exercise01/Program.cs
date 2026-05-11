4bbb478a1a168d68035ca74d24b400c21b6cf557
坂本耀世 <oibc37272@ojs.ac.jp>
2026-05-11 11:36:38 +0900

    2-1終了

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            // 2.1.3
+            var songs = new Song[] {
+            new Song("Let it be", "The Beatles", 243),
+            new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+            new Song("Close To You", "Carpenters", 276),
+            new Song("Honesty", "Billy Joel", 231),
+            new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            PrintSongs(songs);
+        }
+
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Title}{song.ArtistName}{song.Length / 60 + ":" + song.Length % 60}");
+            }
+        }
+    }
+}
