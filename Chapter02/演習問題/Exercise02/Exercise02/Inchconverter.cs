0354d85b9f962079be963b01979cf4a6de025ec7
坂本耀世 <oibc37272@ojs.ac.jp>
2026-05-13 11:02:44 +0900

    インチからメートル変更

---
+++ Chapter02/演習問題/Exercise02/Exercise02/Inchconverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class InchConverter {
+        private static readonly double ratio = 0.0254;  //定数
+        public static double ToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
--- Chapter02/演習問題/Exercise02/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace Exercise02 {
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            PrintInchToMeterList(1, 10);
+        }
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int end) {
+
+            for (int inch = start; inch <= end; inch++) {
+                double merter = InchConverter.ToMeter(inch);
+                Console.WriteLine($"{inch}mm = {merter:0.0000}m");
+            }
         }
     }
 }
