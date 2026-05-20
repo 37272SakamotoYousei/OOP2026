ca6d116a053cd0087f1a7ce09c3ecde3771ae42b
坂本耀世 <oibc37272@ojs.ac.jp>
2026-05-20 12:02:45 +0900

    問題3.1.1完成

---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers) {
+            var num = numbers.Exists(s => s / 8 == 0 || s / 9 == 0);
+            Console.WriteLine();
+        }
+
+        private static void Exercise2(List<int> numbers) {
+            foreach(var num in numbers) {
+                Console.Write(num / 2.0);
+                Console.WriteLine();
+            }
+        }
+
+        private static void Exercise3(List<int> numbers) {
+            var num = numbers.Where(s => s >= 50);
+            Console.WriteLine();
+        }
+
+        private static void Exercise4(List<int> numbers) {
+        }
+    }
+}
