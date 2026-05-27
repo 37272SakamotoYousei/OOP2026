626bc9c7f3cbc4ff11a30350b6313ca199a01031
坂本耀世 <oibc37272@ojs.ac.jp>
2026-05-27 10:22:00 +0900

    4-1-1完成

---
+++ Chapter04/nullに関するイディオム/Section04/Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+namespace Section04 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            #region nullの判定
+            string? name = null;
+
+            if (name is null) {
+                Console.WriteLine("nameはnullです");
+            }
+            #endregion
+
+            #region null合体演算子   
+            string code = "12345";
+            //GetMessage()メソッドの戻り値がnullだったら、
+            //DefaultMessage()メソッドが実行される。
+            var message = GetMessage(code) ?? DefaultMessage();
+            #endregion
+
+            #region null合体代入演算子
+            message = null;
+            message ??= DefaultMessage();
+            #endregion
+
+            #region null条件演算子
+            Sale? sale = new Sale {
+                ShopName = "新宿店",
+                ProductCategory = "洋菓子",
+                Amount = 523100,
+            };
+
+            int? amount = sale?.Amount;
+            Console.WriteLine("売上高:" + amount);
+            #endregion
+
+            #region 2つの要素を入れ替える
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            Console.Write("入れ替え前:");
+            Console.WriteLine("a=" + a + "b=" + b);
+
+            (b, a, c) = (a, b, 999);  //入れ替え
+
+
+            Console.Write("入れ替え後:");
+            Console.WriteLine("a=" + a + "b=" + b);
+            #endregion
+        }
+
+        private static object? DefaultMessage() {
+            return "DefaultMessage";
+        }
+
+        private static object? GetMessage(string code) {
+            return null;
+        }
+    }
+
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; } = string.Empty;
+        //商品カテゴリ
+        public string ProductCategory { get; set; } = string.Empty;
+        //売上高
+        public int Amount { get; set; }
+    }
+}
---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+            "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            Console.WriteLine("foreach文で出力");
+            var name = langs.Where(s => s.Contains('S'));
+            foreach (var n in name) {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("\nfor文で出力");
+            for (int i = 0; i < 9; i++) {
+                if (langs[i].Contains('S')) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+
+            Console.WriteLine("\nwhile文で出力");
+            int cnt = 0;
+            while (cnt < langs.Count) {
+                if (langs[cnt].Contains('S')) {
+                    Console.WriteLine(langs[cnt]);
+                }
+                cnt++;
+            }
+        }
+
+        private static void Exercise2(List<string> langs) {
+
+
+        }
+
+        private static void Exercise3(List<string> langs) {
+
+
+        }
+    }
+}
