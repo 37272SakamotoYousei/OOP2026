b80929efbc0d2c75bd476ec5803298ae145b538b
坂本耀世 <oibc37272@ojs.ac.jp>
2026-06-01 11:57:43 +0900

    5.1.3完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,11 +15,21 @@ namespace Exercise01 {
             Year = year;
             Month = month;
         }
-        //5.2.2
+        //5.1.2
         public bool IsCentury => 2001 <= Year && Year <= 2100;
-        //5.2.3
-        /*public YearMonth AddOneMonth() {
-
-        }*/
+        //5.1.3
+        public YearMonth AddOneMonth() {
+            if(Month == 12) {
+                var num = new YearMonth( Year + 1,1);
+                    return num;
+            } else {
+                var num = new YearMonth(Year, Month + 1);
+                return num;
+            }
+        }
+        //5.1.4
+        public override string ToString() {
+            return base.ToString();
+        }
     }
 }
