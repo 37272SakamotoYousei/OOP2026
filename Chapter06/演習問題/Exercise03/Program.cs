8030cbdedb1dee92b10186cc48376362a1fc7f98
坂本耀世 <oibc37272@ojs.ac.jp>
2026-06-10 10:57:38 +0900

    6.3.1完成

---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Xml.Linq;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise1(string text) {
+            var cnt = text.Count(c => c == ' ');
+            Console.WriteLine(cnt);
+        }
+
+        private static void Exercise2(string text) {
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3(string text) {
+            
+        }
+
+        private static void Exercise4(string text) {
+            
+        }
+
+        private static void Exercise5(string text) {
+            
+        }
+
+        private static void Exercise6(string text) {
+            
+        }
+    }
+}
