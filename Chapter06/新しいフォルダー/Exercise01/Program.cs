5911130fe5394bfd63d3d1cb7fed66322d8d6cb0
坂本耀世 <oibc37272@ojs.ac.jp>
2026-06-10 10:30:06 +0900

    6.2完成

---
+++ Chapter06/新しいフォルダー/Exercise01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+
+            var cultureInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(a, b, cultureInfo,
+                                    CompareOptions.IgnoreWidth | CompareOptions.IgnoreCase) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("数字文字列:");
+            if (int.TryParse(Console.ReadLine(), out var num)) {
+                var number = num.ToString("#,0");
+                Console.WriteLine(number);
+            } else {
+                Console.WriteLine("整数文字列ではありません");
+            }
+
+        }
+    }
+}
