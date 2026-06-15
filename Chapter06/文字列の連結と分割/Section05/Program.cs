340d821d52ccb708e42c35521e0120fe7f8b9713
坂本耀世 <oibc37272@ojs.ac.jp>
2026-06-15 10:36:02 +0900

    文字列の連結

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var c = ",";
+            var result = String.Join(c, GetWords());
+            Console.WriteLine(result);
+        }
+
+        private static IEnumerable<object> GetWords() {
+            return ["Orannge", "Lemon", "Strrawberry"];
+        }
+    }
+}
