be27a196e871b9b6351d0d8926130883640ae3f7
坂本耀世 <oibc37272@ojs.ac.jp>
2026-06-22 11:30:09 +0900

    設問１_Studentクラス完成

---
+++ TestProject/Test01_01/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace Test01_01 {
+    public class Student {
+        public string Name { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public int Score { get; set; }
+    }
+}
