3f50c4d70e73a13807b67bdd68d712d9889fc757
坂本耀世 <oibc37272@ojs.ac.jp>
2026-06-25 15:19:03 +0900

    ディクショナリ演習問題完成

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace Section01 {
+    internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
+
+        static void Main(string[] args) {
+            string? pref, prefCaptalLocation;
+
+            Console.WriteLine("県庁所在地の登録【入力終了:Ctrl + 'Z'】");
+            while (true) {
+                //①都道府県の入力
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+
+                if (pref == null) break;
+
+                //②県庁所在地登録処理
+                Console.Write("県庁所在地:");
+                prefCaptalLocation = Console.ReadLine();
+                Console.WriteLine();
+
+                //③県庁所在地登録処理
+                prefOfficeDict.Add(pref, prefCaptalLocation);
+            }
+
+            while (true) {
+                Console.WriteLine("****めにゅー****");
+                Console.WriteLine("1:一覧表示");
+                Console.WriteLine("2:検索");
+                Console.WriteLine("3:終了");
+                Console.Write(">");
+
+                var aou = Console.ReadLine();
+                if (aou == "3") {
+                    break;
+                }
+
+                switch (aou) {
+                    case "1":
+                        foreach (var item in prefOfficeDict) {
+                            Console.WriteLine($"{item.Key}の県庁所在地は{item.Value}です");
+                        }
+                        break;
+
+                    case "2":
+                        Console.Write("けんさく:");
+                        var search = Console.ReadLine();
+                        var result = prefOfficeDict[search];
+                        Console.WriteLine($"{search}の県庁所在地は{result}です");
+                        break;
+
+                    default:
+                        Console.WriteLine("正しい値ではありません");
+                        break;
+                }
+            }
+        }
+    }
+}
