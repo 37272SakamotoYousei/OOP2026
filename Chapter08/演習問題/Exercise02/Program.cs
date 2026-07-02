badea212e76f887c743cec7dc16c818fc8fe835c
坂本耀世 <oibc37272@ojs.ac.jp>
2026-07-02 14:19:45 +0900

    8.2.4完成

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+
+            // Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核拡散防止条約");
+
+            // 8.2.3 (Countの呼び出し例)
+            // 上のAddメソッドで、２つのオブジェクトを追加しているので、読み込んだ単語数+2が、Countの値になる。
+            Console.WriteLine("件数:" + abbrs.Count);
+
+            Console.WriteLine();    //改行
+
+            // 8.2.3 (Removeの呼び出し例)
+            abbrs.Remove(Console.ReadLine());
+            Console.WriteLine(abbrs.Count);
+
+
+
+
+
+            // すでに削除してあるので、falseが返る
+            if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            // 8.2.4
+            // 新たなGetAllメソッドを追加済みなので、使用してLINQで処理を行う
+            foreach (var word in abbrs.GetAll().Where(x => x.Key.Length == 3)) {
+                Console.WriteLine(word);
+            }
+
+
+
+
+        }
+    }
+}
