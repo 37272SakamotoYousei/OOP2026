3cf2d519d15f5208c7036345988079dfc9bc4596
坂本耀世 <oibc37272@ojs.ac.jp>
2026-07-06 14:14:20 +0900

    p198まで

---
+++ Chapter09/Section01/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Section01 {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            dtpDate = new DateTimePicker();
+            btGet = new Button();
+            tbOut = new TextBox();
+            SuspendLayout();
+            // 
+            // dtpDate
+            // 
+            dtpDate.Font = new Font("Yu Gothic UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            dtpDate.Location = new Point(74, 37);
+            dtpDate.Name = "dtpDate";
+            dtpDate.Size = new Size(200, 43);
+            dtpDate.TabIndex = 0;
+            dtpDate.ValueChanged += dtpDate_ValueChanged;
+            // 
+            // btGet
+            // 
+            btGet.Location = new Point(302, 57);
+            btGet.Name = "btGet";
+            btGet.Size = new Size(75, 23);
+            btGet.TabIndex = 1;
+            btGet.Text = "取得";
+            btGet.UseVisualStyleBackColor = true;
+            btGet.Click += btGet_Click;
+            // 
+            // tbOut
+            // 
+            tbOut.Location = new Point(74, 113);
+            tbOut.Name = "tbOut";
+            tbOut.Size = new Size(250, 23);
+            tbOut.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1473, 644);
+            Controls.Add(tbOut);
+            Controls.Add(btGet);
+            Controls.Add(dtpDate);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dtpDate;
+        private Button btGet;
+        private TextBox tbOut;
+    }
+}
